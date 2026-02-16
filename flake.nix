fed020b96c19b75f1832276058b8765d08578a9b
Gean Ferreira <geanoficial14@gmail.com>
2026-02-16 12:41:04 -0300

    Atualizar a versão dos pacotes do nix e home-manager

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Hyprland clean setup with Home Manager and GDM session";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
