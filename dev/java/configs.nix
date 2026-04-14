cbadcfb05ec7a4c507afccc1e4e6ce61062f1a09
ferreira-gn <geanoficial14@gmail.com>
2026-04-14 16:24:19 -0300

    Alterar a versão do java para 21

--- dev/java/configs.nix
+++ dev/java/configs.nix
@@ -4,10 +4,10 @@
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
-    (maven.override { jdk_headless = pkgs.javaPackages.compiler.openjdk25; })
+    (maven.override { jdk_headless = pkgs.javaPackages.compiler.openjdk21; })
   ];
 
   home.sessionVariables = {
-    JAVA_HOME = "${pkgs.javaPackages.compiler.openjdk25}";
+    JAVA_HOME = "${pkgs.javaPackages.compiler.openjdk21}";
   };
 }
