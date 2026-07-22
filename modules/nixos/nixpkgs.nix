90e67572550409214e9c25301c9650743c1a7cab
ferreira-gn <geanoficial14@gmail.com>
2026-07-22 17:04:00 -0300

    refactor: add home-manager cli to system modules

--- modules/nixos/nixpkgs.nix
+++ modules/nixos/nixpkgs.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 
 {
   nixpkgs.config = {
     allowUnfree = true;
   };
+
+  environment.systemPackages = [
+    inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
 }
